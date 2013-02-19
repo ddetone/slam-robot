9599930074530526f6dccd2a0f469cb74328bd0f
ibrahimmusba <imusba@umich.edu>
2013-02-18 20:33:51 -0500

    Publisher and listener

--- java/src/PandaListener.java
+++ java/src/PandaListener.java
@@ -16,13 +16,42 @@ import botlab.lcmtypes.*;
 
 import lcm.lcm.*;
 
-public class PandaListener
+public class PandaListener implements LCMSubscriber
 {
 	
 	LCM lcm = LCM.getSingleton();
+	bot_status_t bot_status;
 
+	public void messageReceived(LCM lcm, String channel, LCMDataInputStream dins)
+	{
+		try
+		{
+			if(channel.equals("6_POSE"))
+			{
+				bot_status = new bot_status_t(dins);
+				System.out.println("utime:" + bot_status.utime);
+				System.out.println("X:" + bot_status.xyt[0]);
+				System.out.println("Y:" + bot_status.xyt[1]);
+				System.out.println("T:" + bot_status.xyt[2]);
+				System.out.println();
 
-	
-	
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		PandaListener pl = new PandaListener();
+
+		/* Subscribe to 6_POSE */
+		LCM.getSingleton().subscribe("6_POSE", pl);
+								  //      LCM.getSingleton().subscribe("BALLS_LIST", gui);
+									
+	}
+
 
 }
---
+++ java/src/PandaPublisher.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import april.jcam.*;
+import april.util.*;
+import april.jmat.*;
+import april.vis.*;
+import april.image.*;
+import april.jmat.geom.*;
+
+import botlab.lcmtypes.*;
+
+import lcm.lcm.*;
+
+public class PandaPublisher 
+{
+	
+	LCM lcm = LCM.getSingleton();
+	bot_status_t bot_status;
+
+
+	public void run()
+	{
+		int x = 0, y = 0, t = 0;
+
+		while(true)
+		{
+			bot_status_t bot_status = new bot_status_t();
+			bot_status.utime = TimeUtil.utime();
+			bot_status.xyt[0] = x++;
+			bot_status.xyt[1] = y++;
+			bot_status.xyt[2] = t++;
+
+			lcm.publish("6_POSE",bot_status);
+			try{
+				Thread.sleep(33);
+			}catch(InterruptedException e)
+			{}
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		PandaPublisher pp = new PandaPublisher();
+	
+		pp.run();
+		//lcm.subscribe("6_POSE", pl);
+									
+	}
+
+
+}
