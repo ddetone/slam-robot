91034fac58651fee9ef4aa663ba1108a56f787bd
imusba <imusba@umich.edu>
2013-02-23 00:21:37 -0500

    PoseGenerator hopefully working right

--- java/src/botlab/PoseGenerator.java
+++ java/src/botlab/PoseGenerator.java
@@ -20,9 +20,29 @@ import lcm.lcm.*;
 
 public class PoseGenerator implements LCMSubscriber
 {
-	
-	static LCM lcm = LCM.getSingleton();
+	//This class would publish pose messages periadically
+
+	LCM lcm;
 	motor_feedback_t motors;
+
+    public static int[] encoder_curr = new int[2];
+    public static int[] encoder_prev = new int[2];
+
+    //pose global
+    //public static double poseG[] = new double[3];
+    public static bot_status_t bot = new bot_status_t();
+
+    //Determined emperically
+    static final double metersPerTick = 0.000194;
+    static final double base = 18.8/100.0;
+
+    static boolean first = true;
+
+    PoseGenerator()
+    {
+        this.lcm = LCM.getSingleton();
+		lcm.subscribe("MOTOR_FEEDBACK", this);
+    }
 
 	public void messageReceived(LCM lcm, String channel, LCMDataInputStream dins)
 	{
@@ -31,7 +51,8 @@ public class PoseGenerator implements LCMSubscriber
 			if(channel.equals("MOTOR_FEEDBACK"))
 			{
 				motors = new motor_feedback_t(dins);
-				//generatePose();
+				generatePose();
+
 //				System.out.println("utime:" + bot_status.utime);
 //				System.out.println("X:" + bot_status.xyt[0]);
 //				System.out.println("Y:" + bot_status.xyt[1]);
@@ -46,13 +67,66 @@ public class PoseGenerator implements LCMSubscriber
 		}
 	}
 
-	public static void main(String[] args)
+    public void generatePose()
+    {
+
+        encoder_curr[0] = motors.encoders[0];
+        encoder_curr[1] = motors.encoders[1];
+
+        if(first)
+        {
+            encoder_prev[0] = encoder_curr[0];
+            encoder_prev[1] = encoder_curr[1];
+            first = false;
+        }
+
+        double encoder_delta[] = new double[]{-(encoder_curr[0] - encoder_prev[0]), (encoder_curr[1] - encoder_prev[1])};
+
+        double x_T = (encoder_delta[1] + encoder_delta[0]) / 2 * metersPerTick;
+        double y_T = 0;
+        double t_T = (encoder_delta[1] - encoder_delta[0]) / base * metersPerTick;
+
+        encoder_prev[0] = encoder_curr[0];
+        encoder_prev[1] = encoder_curr[1];
+
+
+        //pose global
+        double x_A = bot.xyt[0];
+        double y_A = bot.xyt[1];
+        double t_A = bot.xyt[2];
+
+        double ca = Math.cos(t_A);
+        double sa = Math.sin(t_A);
+
+        double x_B = x_T*ca - y_T*sa + x_A;
+        double y_B = x_T*sa + y_T*ca + y_A;
+        double t_B = t_A + t_T;
+
+
+        bot.xyt[0] = x_B;
+        bot.xyt[1] = y_B;
+        bot.xyt[2] = t_B;
+
+        bot.utime = TimeUtil.utime();
+
+        lcm.publish("6_POSE",bot);
+        try{
+            Thread.sleep(33);
+        }catch(InterruptedException e)
+        {}
+
+
+    }
+
+
+	public static void main(String[] args) throws Exception
 	{
-		PoseGenerator pl = new PoseGenerator();
+		PoseGenerator pg = new PoseGenerator();
 
 		/* Subscribe to 6_POSE */
-		lcm.subscribe("6_POSE", pl);
-		while(true);
+		while(true){
+            Thread.sleep(1000);
+        }
 	}
 
 
