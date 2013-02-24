5754ff0a580976a845c86a72e09c35f351539050
Jonathan Bendes <jbendes@umich.edu>
2013-02-24 00:10:05 -0500

    working on PandaRunRemote

--- scripts/PandaRunRemote.sh
+++ scripts/PandaRunRemote.sh
@@ -26,7 +26,7 @@ echo "Starting procman on panda..."
 
 LCM_LOGFILE="\$BOTLAB_HOME/data/$(date -u +%m"."%d"."%g"."%H"."%M)"
 PROCMAN_CMD="/home/panda-user/botlab/scripts/runproc_panda.sh"
-PROCMAN_CMD="bash -e \$BOTLAB_HOME/scripts/runproc_panda.sh"
+#PROCMAN_CMD="bash -e \$BOTLAB_HOME/scripts/runproc_panda.sh"
 
 set -x
 ssh $1 "$PROCMAN_CMD"
