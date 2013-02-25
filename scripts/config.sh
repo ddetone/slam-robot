9d0491266ff8e446ac9a32886c9facb71bbb3e02
Jonathan Bendes <jbendes@umich.edu>
2013-02-24 22:00:39 -0500

    more lcm logs and a pull data script

---
+++ scripts/config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# $1 should be the hostname
+
+if [ -z $1 ]; then
+	echo "no host specified (first argument value)"
+	exit
+fi
+
+cd $BOTLAB_HOME/java
+ant clean; ant
+
+ssh $1 mkdir -p $2botlab
+
+scp -pr $BOTLAB_HOME/config $1:$2botlab/
+scp -pr $BOTLAB_HOME/java/botlab.jar $1:$2botlab/
+scp -pr $BOTLAB_HOME/lcmtypes $1:$2botlab/
+scp -pr $BOTLAB_HOME/scripts $1:$2botlab/
+ssh $1 mkdir -p $2botlab/data
+
