10b71983a035db55077de832300d12d438c386ff
Jonathan Bendes <jbendes@umich.edu>
2013-03-06 04:09:35 -0500

    cant tell if homography is working. It might be. Error seems strangely high if it is working

--- java/src/botlab/CameraCalibration.java
+++ java/src/botlab/CameraCalibration.java
@@ -17,55 +17,47 @@ import april.jmat.geom.*;
 
 public class CameraCalibration
 {
-	
-	public static double[][] getJacobian(double f, double cx, double cy, double phi, double Tx, double Ty, double Tz, double X, double Y, double Z){
+
+
+	public static double[][] J_Cross(double cx, double cy, double x, double y){
 		
-		double[][] J = {{Tx + X,
-				Tz + Z*Math.cos(phi) - Y*Math.sin(phi),
-				0,
-				-Z*cx*Math.sin(phi) - Y*cx*Math.cos(phi), 
-				f, 
-				0, 
-				cx},
-				{Ty + Y*Math.cos(phi) + Z*Math.sin(phi),
-				0,
-				Tz + Z*Math.cos(phi) - Y*Math.sin(phi),
-				Z*(f*Math.cos(phi) - cy*Math.sin(phi)) - Y*(cy*Math.cos(phi) + f*Math.sin(phi)),
-				0,
-				f,
-				cy},
-				{0,
-				0,
-				0,
-				-Y*Math.cos(phi) - Z*Math.sin(phi), 
-				0, 
-				0,
-				1}};
-		return J;
+		double cw = 1;
+		
+		double[][] jacobian = new double[][]{
+			{     0,     0,   0, -cw*x, -cw*y, -cw,  cy*x,  cy*y,  cy},
+			{  cw*x,  cw*y,  cw,     0,     0,   0, -cx*x, -cx*y, -cx},
+			{ -cy*x, -cy*y, -cy,  cx*x,  cx*y,  cx,     0,     0,   0}};
+
+		return jacobian;
 		
 	}
 
 
 	public static void main(String args[]){
-		Matrix A = new Matrix(36,7);
-		Matrix b = new Matrix(36,1);
-		Matrix x = new Matrix(7,1);
+		double Cirx = 3.37025048258259540e+02; // larger -> moves right
+		double Ciry = 2.4675008449138741e+02; // larger -> moves down
+		int numCorrespondences = 12;
+		Matrix A = new Matrix(9,9);
+		Matrix J = new Matrix(3 * numCorrespondences,9);
+		Matrix h = new Matrix(9,1);
+		Matrix H = new Matrix(3,3);
 		
 		ArrayList<double[]> points = new ArrayList<double[]>();
 		ArrayList<double[]> pixels = new ArrayList<double[]>();
 		
-		points.add(new double[]{0,0,2});
-		points.add(new double[]{0,0,3});
-		points.add(new double[]{0,0,4});
-		points.add(new double[]{0,0,5});
-		points.add(new double[]{1,0,2});
-		points.add(new double[]{1,0,4});
-		points.add(new double[]{2,0,3});
-		points.add(new double[]{0.5,0,3});
-		points.add(new double[]{-1,0,2});
-		points.add(new double[]{-1,0,4});
-		points.add(new double[]{-2,0,5});
-		points.add(new double[]{-0.5,0,3});
+		points.add(new double[]{2,0});
+		points.add(new double[]{3,0});
+		points.add(new double[]{4,0});
+		points.add(new double[]{5,0});
+		points.add(new double[]{2,1});
+		points.add(new double[]{4,1});
+		points.add(new double[]{3,2});
+		points.add(new double[]{3,0.5});
+		points.add(new double[]{2,-1});
+		points.add(new double[]{4,-1});
+		points.add(new double[]{5,-2});
+		points.add(new double[]{3,-0.5});
+
 		pixels.add(new double[]{620,689});
 		pixels.add(new double[]{623,610});
 		pixels.add(new double[]{623,571});
@@ -80,56 +72,47 @@ public class CameraCalibration
 		pixels.add(new double[]{511,609});
 
 		assert(pixels.size() == points.size());
-		int numCorrespondences = pixels.size();
-		final int numIterations = 10000;
-		
 		for(int i = 0; i < points.size(); i++){
 			points.get(i)[0] *= 0.3048;
-			//points.get(i)[1] *= 0.3048;
-			points.get(i)[2] *= 0.3048;
+			points.get(i)[1] *= 0.3048;
+			// not necessary since should be all 0s.
+			//points.get(i)[2] *= 0.3048;
+			pixels.get(i)[0] -= Cirx;
+			pixels.get(i)[1] -= Ciry;
 		}
-
-		//set x
-		//for(int i = 0; i < 7; i++){
-		//	x.set(i,0,);
-		//}
-		x.set(0,0,0.015);
-		x.set(3,0,-0.1);
-		x.set(6,0,0.21);
 		
-		//set b
 		for(int i = 0; i < numCorrespondences; i++){
-			b.set(3 * i, 0, new double[][]{	{pixels.get(i)[0]},
-							{pixels.get(i)[1]},
-							{1}});
+			J.set(3 * i, 0, J_Cross(points.get(i)[0], points.get(i)[1], pixels.get(i)[0], pixels.get(i)[1]));
 		}
 		
+		A = J.transpose().times(J);
+
+		SingularValueDecomposition svd = new SingularValueDecomposition(A);
+		System.out.println(svd.getV().getRowDimension());
+		System.out.println(svd.getV().getColumnDimension());
+		double[][] htemp = svd.getV().copyArray(0, 8, 9, 1);
+		H = new Matrix(new double[][]{
+			{htemp[0][0], htemp[1][0], htemp[2][0]},
+			{htemp[3][0], htemp[4][0], htemp[5][0]},
+			{htemp[6][0], htemp[7][0], htemp[8][0]}});
 		
-		try{
-			for(int i = 0; i < numIterations; i++){
-				System.out.println(i + "th time in loop");
-				for(int j = 0; j < numCorrespondences; j++){
-					A.set(3 * j, 0, getJacobian(x.get(0,0),
-								x.get(1,0),
-								x.get(2,0),
-								x.get(3,0),
-								x.get(4,0),
-								x.get(5,0),
-								x.get(6,0),
-								points.get(j)[0],
-								points.get(j)[1],
-								points.get(j)[2]));
-				}
-				Matrix At = A.transpose();
-				Matrix AtA = At.times(A);
-				Matrix AtAinv = AtA.inverse();
-				Matrix Atb = At.times(b);
-				x = AtAinv.times(Atb);
-			}
-		}catch(RuntimeException e){
+		
+		H.print();
+		for(int i = 0; i < numCorrespondences; i++){
+			Matrix test = Matrix.columnMatrix(new double[]{pixels.get(i)[0], pixels.get(i)[1], 1});
+			test = H.times(test);
+			Matrix test1 = Matrix.columnMatrix(new double[]{test.get(0,0) / test.get(2,0), test.get(1,0) / test.get(2,0)});
+			Matrix point = Matrix.columnMatrix(new double[]{points.get(i)[0], points.get(i)[1]});
+			//test1.print();
+			//point.print();
+			point.minusEquals(test1);
+			//point.print();
+			System.out.println(Math.sqrt(point.transpose().times(point).get(0,0)));
+			//System.out.println();
+			//System.out.println();
+			//System.out.println();
+			//System.exit(1);
 		}
-		
-		x.print();
 		
 	}
 	
