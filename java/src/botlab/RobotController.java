831a75269167a3ac14493139edf014f4ebc5d475
Andrew Segavac <asegavac@umich.edu>
2013-03-18 03:41:33 -0400

    Added shooting logic

--- java/src/botlab/RobotController.java
+++ java/src/botlab/RobotController.java
@@ -24,12 +24,17 @@ public class RobotController implements LCMSubscriber
 	LCM lcm;
 	bot_status_t bot_status;
 	botlab.PoseTracker tracker;
+	boolean finished = false;
+	boolean done_searching = false;
 	map_t map;
 	String state;
 	xyt_t robotPose;
+	slam_vector_t slam_vec;
+	map_features_t features;
 
 	int pastNumTriangles;
-	LinkedList<Integer> trianglesToKill;
+	ArrayList<Double> killedTriangles;
+	double triangle_to_kill;
 
 	RobotController()
 	{
@@ -41,13 +46,125 @@ public class RobotController implements LCMSubscriber
 		tracker = botlab.PoseTracker.getSingleton();
 		map = null;
 		robotPose = null;
+		slam_vec = null;
+		features = null;
+
 		state = "explore";
 		pastNumTriangles = 0;
-		trianglesToKill = new LinkedList<Integer>();
+		killedTriangles = new ArrayList<Double>();
+
 		lcm.subscribe("6_MAP",this);
+		lcm.subscribe("6_POSE",this);
+		lcm.subscribe("6_FEATURES",this);
 		lcm.subscribe("6_SLAM_POSES",this);
 	}
 	
+	
+	
+	
+	public void messageReceived(LCM lcm, String channel, LCMDataInputStream dins)
+	{
+		try
+		{
+			if(finished){
+				return;
+			}
+
+			if(channel.equals("6_MAP")){
+				map = new map_t(dins);
+				if(robotPose != null && bot_status != null && features != null){
+					if(state == "explore"){
+						if(Math.abs(robotPose.xyt[0]) < 0.10 && Math.abs(robotPose.xyt[1]) < 0.10 && done_searching){
+							state = "spinning";
+						}
+						if(slam_vec.numTriangles > killedTriangles.size()){
+							double min_dist = Double.MAX_VALUE;
+							for(int i = 0; i < slam_vec.numTriangles; ++i){
+								if(!killedTriangles.contains(new Double(slam_vec.triangles[i][3]))){
+									double dist = LinAlg.distance(slam_vec.triangles[i],robotPose.xyt,2);
+									if(dist < min_dist){
+										min_dist = dist;
+										state = "moving to point";
+										triangle_to_kill = slam_vec.triangles[i][3];
+									}
+									
+								}
+							}
+						} else {
+							planFrontier();
+						}
+					}
+					if(state == "moving to point"){
+						double[] triangle = null;
+						for(int i = 0; i < slam_vec.numTriangles; ++i){
+							if(triangle_to_kill == slam_vec.triangles[i][3]){
+								triangle = slam_vec.triangles[i];
+							}
+						}
+						if(LinAlg.distance(robotPose.xyt, triangle,2) < 1.0) { //1 meters shooting distance
+							state = "aligning to triangle";
+							//calculate angle to where slam says it is
+							double angle = Math.atan2((triangle[1] - robotPose.xyt[1]),(triangle[0] - robotPose.xyt[0]));
+							xyt_t alignment_goal = new xyt_t();
+							alignment_goal.xyt = new double[]{robotPose.xyt[0], robotPose.xyt[1], angle};
+							//publish new goal
+							lcm.publish("6_GOAL", alignment_goal);
+						} else {
+							xyt_t new_goal = new xyt_t();
+							new_goal.xyt = new double[]{triangle[0], triangle[1], triangle[2]};
+							lcm.publish("6_GOAL", new_goal);
+						}
+					}
+				}
+			} else if(channel.equals("6_SLAM_POSES")){
+				slam_vec = new slam_vector_t(dins);
+				robotPose = slam_vec.xyt[slam_vec.numPoses - 1];
+			} else if(channel.equals("6_POSE")){
+				bot_status = new bot_status_t(dins);
+				if(state == "spinning"){
+					//TODO: spinning logic
+					//TODO: if done spinning
+						//TODO: finished
+				}
+			} else if(channel.equals("6_FEATURES")){
+				features = new map_features_t(dins);
+				if(state == "aligning to triangle"){
+					double min_dist = Double.MAX_VALUE;
+					int min_triangle = 0;
+					for(int i = 0; i < features.ntriangles; ++i){
+						if(LinAlg.distance(new double[]{0.0,0.0},features.triangles[i],2) < min_dist){
+							min_dist = LinAlg.distance(new double[]{0.0,0.0},features.triangles[i],2);
+							min_triangle = i;
+						}
+					}
+					if(min_dist != Double.MAX_VALUE){
+						double angle = Math.atan2(features.triangles[min_triangle][1], features.triangles[min_triangle][0]);
+						//TODO: if angle dead on
+						if(angle < 0.02){
+							//shoot laser
+							Laser laser = new Laser();
+							laser.shoot();
+							//add triangle_to_kill to killed list
+							killedTriangles.add(new Double(triangle_to_kill));
+							state = "explore";
+						}
+						else{
+							xyt_t new_angle = new xyt_t();
+							new_angle.xyt = new double[]{bot_status.xyt[0],bot_status.xyt[1],bot_status.xyt[2]+angle};
+
+							//publish waypoint of angle
+							lcm.publish("6_GOAL", new_angle);
+						}
+					}
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 	public void planFrontier(){
 		//frontier planner
 		if(state == "explore"){
@@ -132,6 +249,7 @@ public class RobotController implements LCMSubscriber
 			if(max_size < 0.45/map.scale){ //map explored return to start
 				goal_x = 0;
 				goal_y = 0;
+				done_searching = true;
 			}
 
 			xyt_t goal = new xyt_t();
@@ -142,32 +260,6 @@ public class RobotController implements LCMSubscriber
 		}	
 	}
 	
-	
-	
-	public void messageReceived(LCM lcm, String channel, LCMDataInputStream dins)
-	{
-		try
-		{
-			if(channel.equals("6_MAP")){
-				map = new map_t(dins);
-				if(robotPose != null) planFrontier();
-				/*for(int i = pastNumTriangles; i < map.numTriangles; ++i) {
-					trianglesToKill.add(new Integer(i));
-				}
-				if(trianglesToKill.size() > 0){
-					state = "Shoot";
-					//do shooting stuff
-				}*/
-			} else if(channel.equals("6_SLAM_POSES")){
-				slam_vector_t slam_vec = new slam_vector_t(dins);
-				robotPose = slam_vec.xyt[slam_vec.numPoses - 1];
-			}
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
 
 	public static void main(String[] args) throws Exception
 	{
